e47f0e994df45d8099646d6be708fd30a491b279
Jarvis-M <1766531162@qq.com>
2025-10-10 09:29:43 +0800

    [Feat] 基础OCR

---
+++ PaddleOCR.py
@@ -0,0 +1,15 @@
+from paddleocr import PaddleOCR
+
+ocr = PaddleOCR(
+    text_detection_model_name="PP-OCRv5_server_det",
+    text_recognition_model_name="PP-OCRv5_server_rec",
+    use_doc_orientation_classify=False,
+    use_doc_unwarping=False,
+    use_textline_orientation=False,
+)
+
+output = ocr.predict(r'D:\Project\Automatic-Grading\1.jpg')
+for res in output:
+    res.print()
+    res.save_to_img(save_path="./output/")
+    res.save_to_json(save_path="./output/res.json")
